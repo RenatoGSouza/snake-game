7721a0b9a6fe7f9418340216ad857b38c56496a2
Renato GSouza <renatomacedo1995@gmail.com>
2021-07-24 17:48:37 -0300

    definir reaçoes e evitar da cobra sumir

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById('snake');
 const context = canvas.getContext('2d');
+
 let box = 32;
 
 const snake = [];
@@ -22,25 +23,43 @@ function criarCobrinha() {
   });
 }
 
+document.addEventListener('keydown', update);
+
+function update ({keyCode}) {
+  console.log(keyCode);
+  if(keyCode === 37 && direction !== 'right') direction = 'left';
+  if(keyCode === 38 && direction !== 'down') direction = 'up';
+  if(keyCode === 39 && direction !== 'left') direction = 'right';
+  if(keyCode === 40 && direction !== 'up') direction = 'down';
+}
+
 function iniciarJogo() {
+
+  if(snake[0].x > 15 * box && direction === 'right') snake[0].x = 0;
+  if(snake[0].x < 0 && direction === 'left') snake[0].x = 16 * box;
+  if(snake[0].y > 15 * box && direction === 'right') snake[0].y = 0;
+  if(snake[0].y < 0 && direction === 'left') snake[0].y = 16 * box;
+
+
   criarBG();
   criarCobrinha();
 
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
 
-  switch (direction) {
-    case 'right':
-      snakeX += box;
-    case 'left':
-      snakeX -= box;
-    case 'up':
-      snakeY += box;
-    case 'down':
-      snakeY -= box;    
-    default:
-      snake;
+  if(direction === 'right') snakeX += box;
+  if(direction === 'left') snakeX -= box;
+  if(direction === 'up') snakeY -= box;
+  if(direction === 'down') snakeY += box;
+
+  snake.pop();
+
+  const newHead = {
+    x: snakeX,
+    y: snakeY
   }
+
+  snake.unshift(newHead);
 }
 
 const jogo = setInterval(iniciarJogo, 100);
