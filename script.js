f07f415dd6f7a526e123ca0a4e3855fb85995c26
Renato GSouza <renatomacedo1995@gmail.com>
2021-07-24 17:16:21 -0300

    adicionando funçao de movimento da cobra

--- script.js
+++ script.js
@@ -1,23 +1,46 @@
 const canvas = document.getElementById('snake');
 const context = canvas.getContext('2d');
 let box = 32;
+
 const snake = [];
 snake[0] = {
-    x: 8 * box,
-    y: 8 * box,
+  x: 8 * box,
+  y: 8 * box,
 }
 
+const direction = 'right';
+
 function criarBG() {
-    context.fillStyle = 'lightgreen';
-    context.fillRect(0, 0, 16 * box, 16 * box);
+  context.fillStyle = 'lightgreen';
+  context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 function criarCobrinha() {
-    snake.forEach((snake) => {
-        context.fillStyle = 'green'
-        context.fillRect(snake.x, snake.y, box, box);
-    });
+  snake.forEach((snake) => {
+    context.fillStyle = 'green'
+    context.fillRect(snake.x, snake.y, box, box);
+  });
 }
 
-criarBG();
-criarCobrinha();
+function iniciarJogo() {
+  criarBG();
+  criarCobrinha();
+
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+
+  switch (direction) {
+    case 'right':
+      snakeX += box;
+    case 'left':
+      snakeX -= box;
+    case 'up':
+      snakeY += box;
+    case 'down':
+      snakeY -= box;    
+    default:
+      snake;
+  }
+}
+
+const jogo = setInterval(iniciarJogo, 100);
